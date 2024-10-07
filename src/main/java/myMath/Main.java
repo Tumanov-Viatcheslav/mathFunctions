4f58ce13d78f9e0ef435a88ebc5ad7a0f69ee2d8
Tumanov <tslavich@bk.ru>
2024-10-07 11:17:19 +0300

    Muted logger

--- src/main/java/myMath/Main.java
+++ src/main/java/myMath/Main.java
@@ -1,8 +1,20 @@
 package myMath;
 
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.spi.LoggingEvent;
+
 public class Main {
     public static void main(String[] args) {
         String fileName = "src/main/resources/output.xlsx";
-        PointsWriter.writePointListToExel(PointsFabricator.generatePoints(-10, 10, 100, new Sinus()), fileName);
+        BasicConfigurator.configure(new AppenderSkeleton() {
+            @Override
+            protected void append(LoggingEvent loggingEvent) {}
+            @Override
+            public void close() {}
+            @Override
+            public boolean requiresLayout() {return false;}
+        });
+        PointsWriter.writePointListToExel(PointsFabricator.generatePoints(-10, 10, 100, new LineFunction(3, 2)), fileName);
     }
 }
